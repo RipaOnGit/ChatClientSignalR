4178ef9ca36e75dbf12d4b4cd0ce98cfcf8c56ab
ripcu <ripcu@kolumbus.fi>
2015-05-13 16:38:41 +0300

    Initial version of this demo application.

---
+++ WebRole1/ChatHub.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage;
+
+namespace SignalRChat
+{
+    //Instructions: https://www.macaw.nl/weblog/2013/8/setting-up-website-deployment-to-windows-azure
+    public class ChatHub : Hub
+    {
+         private readonly static ConnectionMapping<string> _connections = 
+            new ConnectionMapping<string>();
+
+        private MessageData dbMessage; 
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+            dbMessage = new MessageData()
+            {
+                message = message,
+                user = name,
+                PartitionKey = "Messages",
+                RowKey = Guid.NewGuid().ToString()
+            };
+            string accountName = "ripasstorage1";
+            string accountKey = "0M78LT+YVu7/omJULHgjTiRvbhQVOoadnI4kpl5ykDESqY1Z/PW5uqEj4pLLX2k1u7k0zQAsFO3PCJduZwcszw==";
+            try
+            {
+                StorageCredentials creds = new StorageCredentials(accountName, accountKey);
+                CloudStorageAccount account = new CloudStorageAccount(creds, useHttps: true);
+
+                CloudTableClient client = account.CreateCloudTableClient();
+
+                CloudTable table = client.GetTableReference("chatMessages");
+                TableOperation tableOperation = TableOperation.Insert(dbMessage);
+                TableResult result = table.Execute(tableOperation);
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+        public void addUser(string User)
+        {
+            _connections.Add(Context.ConnectionId, User);
+            Clients.All.getConnectedUsers(_connections.GetAllConnections());
+        }
+
+        //public override Task OnConnected()
+        //{
+        //    _connections.Add(currentUser, Context.ConnectionId);
+
+        //    return base.OnConnected();
+        //}
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var id = _connections.GetConnections(Context.ConnectionId).FirstOrDefault();
+            _connections.Remove(Context.ConnectionId, id);
+            Clients.All.getConnectedUsers(_connections.GetAllConnections());
+
+            return base.OnDisconnected(stopCalled);
+        }
+
+        //public override Task OnReconnected()
+        //{
+        //    if (!_connections.GetConnections("").Contains(Context.ConnectionId))
+        //    {
+        //        _connections.Add(Guid.NewGuid().ToString(), Context.ConnectionId);
+        //    }
+
+        //    return base.OnReconnected();
+        //}
+    }
+
+    internal class MessageData : TableEntity
+    {
+        public string message { get; set; }
+        public string user { get; set; }
+    }
+
+    /// <summary>
+    /// User Connection realted class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ConnectionMapping<T>
+    {
+        private readonly Dictionary<T, HashSet<string>> _connections =
+            new Dictionary<T, HashSet<string>>();
+
+        public int Count
+        {
+            get
+            {
+                return _connections.Count;
+            }
+        }
+
+        public void Add(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    connections = new HashSet<string>();
+                    _connections.Add(key, connections);
+                }
+
+                lock (connections)
+                {
+                    connections.Add(connectionId);
+                }
+            }
+        }
+
+        public IEnumerable<string> GetConnections(T key)
+        {
+            HashSet<string> connections;
+            if (_connections.TryGetValue(key, out connections))
+            {
+                return connections;
+            }
+
+            return Enumerable.Empty<string>();
+        }
+
+        public List<string> GetAllConnections()
+        {
+            List<string> outs = new List<string>();
+            foreach (var con in _connections)
+            {
+                outs.Add(con.Value.ToList()[0]);
+            }
+            return outs;
+        }
+
+        public void Remove(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                HashSet<string> connections;
+                if (!_connections.TryGetValue(key, out connections))
+                {
+                    return;
+                }
+
+                lock (connections)
+                {
+                    connections.Remove(connectionId);
+
+                    if (connections.Count == 0)
+                    {
+                        _connections.Remove(key);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WebRole1/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            CreateTableIFNotExist();
+            app.MapSignalR();
+        }
+        private void CreateTableIFNotExist()
+        {
+            string accountName = "ripasstorage1";
+            string accountKey = "0M78LT+YVu7/omJULHgjTiRvbhQVOoadnI4kpl5ykDESqY1Z/PW5uqEj4pLLX2k1u7k0zQAsFO3PCJduZwcszw==";
+            try
+            {
+                StorageCredentials creds = new StorageCredentials(accountName, accountKey);
+                CloudStorageAccount account = new CloudStorageAccount(creds, useHttps: true);
+
+                CloudTableClient client = account.CreateCloudTableClient();
+
+                CloudTable table = client.GetTableReference("chatMessages");
+
+                if (!table.Exists(null, null))
+                    table.CreateIfNotExists(); ;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+    }
+}
