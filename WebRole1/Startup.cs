7f95f581a4a442655d440bc9295ac95858731e76
ripcu <ripcu@kolumbus.fi>
2015-05-13 23:18:10 +0300

    Ninject IoC container added to provide depency injection into application.

--- WebRole1/Startup.cs
+++ WebRole1/Startup.cs
@@ -6,6 +6,11 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.AspNet.SignalR;
+using Ninject;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNet.SignalR.Infrastructure;
 
 [assembly: OwinStartup(typeof(SignalRChat.Startup))]
 namespace SignalRChat
@@ -16,12 +21,50 @@ namespace SignalRChat
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
 
-            //Lets register custom hub instance builder.
-            GlobalHost.DependencyResolver.Register(
-            typeof(ChatHub),
-            () => new ChatHub(new DataBaseManager()));
+            //Lets use Ninject container for IoC 
+            var kernel = new StandardKernel();
+            var resolver = new NinjectSignalRDependencyResolver(kernel);
 
-            app.MapSignalR();
+            kernel.Bind<IDataBaseManager>().To<DataBaseManager>().InSingletonScope();  // Make it a singleton object.
+            kernel.Bind(typeof(IHubConnectionContext<dynamic>)).ToMethod(context =>
+                    resolver.Resolve<IConnectionManager>().GetHubContext<ChatHub>().Clients
+                     ).WhenInjectedInto<IDataBaseManager>();
+
+            var config = new HubConfiguration();
+            config.Resolver = resolver;
+            ConfigureSignalR(app, config);
+        }
+
+        /// <summary>
+        /// Configure actual SignalR.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="config"></param>
+        public static void ConfigureSignalR(IAppBuilder app, HubConfiguration config)
+        {
+            app.MapSignalR(config);
+        }
+    }
+
+    /// <summary>
+    /// The Ninject IoC container related support method.
+    /// </summary>
+    internal class NinjectSignalRDependencyResolver : DefaultDependencyResolver
+    {
+        private readonly IKernel _kernel;
+        public NinjectSignalRDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public override object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType) ?? base.GetService(serviceType);
+        }
+
+        public override IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType).Concat(base.GetServices(serviceType));
         }
     }
 }
